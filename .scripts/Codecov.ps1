0241aab4ac79f8d7090610ed721ac8eaf829bb67
Coen Munckhof <c.munckhof@gmail.com>
2018-06-18 19:52:22 +0200

    execute codecov

--- .scripts/Codecov.ps1
+++ .scripts/Codecov.ps1
@@ -15,6 +15,9 @@ $nugetToolsDir = "C:\NuGetTools"
 $codecovExe = (join-path $nugetToolsDir "Codecov.1.0.4\tools\codecov.exe")
 Get-ChildItem -Recurse ($nugetToolsDir) | Where-Object {$_.Name -like "codecov.exe"} | % { $codecovExe = $_.FullName};
 
+if (!Test-Path $codecovExe) {
+  Write-Warning "$codecovExe  DOES NOT EXISTS"
+}
 Write-Host "CodeCov.exe " $codecovExe
 
 $outputOpenCoverXmlFile = (join-path $RootDir "coverage-dotnet.xml")
@@ -31,7 +34,8 @@ $outputOpenCoverXmlFile = (join-path $RootDir "coverage-dotnet.xml")
 # Write-Host "Orig build version: " $build_version_orig
 # Write-Host "New build version: " $env:APPVEYOR_BUILD_VERSION
 # 
-$codecovExe -f $outputOpenCoverXmlFile
+
+iex $codecovExe -f $outputOpenCoverXmlFile
 
 # Write-Host "Restore"
 # $env:APPVEYOR_BUILD_VERSION = $build_version_orig
