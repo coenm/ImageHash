cdcd608af40b858b69c1c7177e0d19eeacb3e97b
Coen Munckhof <c.munckhof@gmail.com>
2018-06-18 19:46:13 +0200

    custom codecov via nuget

--- .scripts/Codecov.ps1
+++ .scripts/Codecov.ps1
@@ -11,23 +11,28 @@ Write-Host "PSScriptRoot: " $PSScriptRoot
 $RootDir = [IO.Path]::GetFullPath( (join-path $PSScriptRoot "..\") )
 Write-Host "ROOT: " $RootDir
 
-# $outputOpenCoverXmlFile = 'C:\projects\coverage-dotnet.xml'
+$nugetToolsDir = "C:\NuGetTools"
+$codecovExe = (join-path $nugetToolsDir "Codecov.1.0.4\tools\codecov.exe")
+Get-ChildItem -Recurse ($nugetToolsDir) | Where-Object {$_.Name -like "codecov.exe"} | % { $codecovExe = $_.FullName};
+
+Write-Host "CodeCov.exe " $codecovExe
+
 $outputOpenCoverXmlFile = (join-path $RootDir "coverage-dotnet.xml")
 
-$branch_orig = $env:APPVEYOR_REPO_BRANCH
-$branch_new = $branch_orig
-$env:APPVEYOR_REPO_BRANCH = $branch_new.Replace("/",".").Replace("-",".")
+# $branch_orig = $env:APPVEYOR_REPO_BRANCH
+# $branch_new = $branch_orig
+# $env:APPVEYOR_REPO_BRANCH = $branch_new.Replace("/",".").Replace("-",".")
 
-$build_version_orig = $env:APPVEYOR_BUILD_VERSION
-$build_version_new = $build_version_orig
-$env:APPVEYOR_BUILD_VERSION = "test"
-#$build_version_new.Replace("+",".")
+# $build_version_orig = $env:APPVEYOR_BUILD_VERSION
+# $build_version_new = $build_version_orig
+# $env:APPVEYOR_BUILD_VERSION = "test"
+# #$build_version_new.Replace("+",".")
+# 
+# Write-Host "Orig build version: " $build_version_orig
+# Write-Host "New build version: " $env:APPVEYOR_BUILD_VERSION
+# 
+$codecovExe -f $outputOpenCoverXmlFile
 
-Write-Host "Orig build version: " $build_version_orig
-Write-Host "New build version: " $env:APPVEYOR_BUILD_VERSION
-
-codecov -f $outputOpenCoverXmlFile
-
-Write-Host "Restore"
-$env:APPVEYOR_BUILD_VERSION = $build_version_orig
-$env:APPVEYOR_REPO_BRANCH = $branch_orig
+# Write-Host "Restore"
+# $env:APPVEYOR_BUILD_VERSION = $build_version_orig
+# $env:APPVEYOR_REPO_BRANCH = $branch_orig
