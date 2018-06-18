329211e19911643f66f8348a567ea602692f1c86
Coen Munckhof <c.munckhof@gmail.com>
2018-06-18 20:37:41 +0200

    Cleanup

--- .scripts/Codecov.ps1
+++ .scripts/Codecov.ps1
@@ -18,27 +18,9 @@ Get-ChildItem -Recurse ($nugetToolsDir) | Where-Object {$_.Name -like "codecov.e
 if (! ( Test-Path $codecovExe )) 
 {
   Write-Warning "$codecovExe  DOES NOT EXISTS"
-  $codecovExe = (join-path $nugetToolsDir "Codecov.1.0.4\tools\codecov.exe")
 }
 
 Write-Host "CodeCov.exe " $codecovExe
 
 $outputOpenCoverXmlFile = (join-path $RootDir "coverage-dotnet.xml")
-
-#$branch_orig = $env:APPVEYOR_REPO_BRANCH
-#$branch_new = $branch_orig
-#$env:APPVEYOR_REPO_BRANCH = $branch_new.Replace("/",".").Replace("-",".")
-
-$build_version_orig = $env:APPVEYOR_BUILD_VERSION
-$build_version_new = $build_version_orig
-$env:APPVEYOR_BUILD_VERSION = "test"
-#$build_version_new.Replace("+",".")
-
-Write-Host "Orig build version: " $build_version_orig
-Write-Host "New build version: " $env:APPVEYOR_BUILD_VERSION
-
 iex "$codecovExe -f $outputOpenCoverXmlFile"
-
-Write-Host "Restore"
-$env:APPVEYOR_BUILD_VERSION = $build_version_orig
-#$env:APPVEYOR_REPO_BRANCH = $branch_orig
