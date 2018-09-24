0fe3c613657f51383e6d275d17df156583529d15
Coen Munckhof <c.munckhof@gmail.com>
2018-09-24 17:07:50 +0200

    Reordering of folders

--- .scripts/pack.ps1
+++ .scripts/pack.ps1
@@ -10,7 +10,7 @@ function NugetPack([bool] $pre)
         echo "Creating stable package..."
     }
     
-    dotnet pack -c Release --include-symbols src\ImageHash.sln
+    dotnet pack -c Release --include-symbols ImageHash.sln
 }
 
 NugetPack ($env:APPVEYOR_REPO_TAG -eq "false")
