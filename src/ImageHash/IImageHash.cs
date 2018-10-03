6d82da582715970d35628dfe84a40e4e2f99b51f
Coen Munckhof <c.munckhof@gmail.com>
2018-10-03 23:09:05 +0200

    typos

--- src/ImageHash/IImageHash.cs
+++ src/ImageHash/IImageHash.cs
@@ -13,7 +13,7 @@
         /// <summary>Hash the image using the algorithm.</summary>
         /// <param name="image">image to calculate hash from</param>
         /// <returns>hash value of the image.</returns>
-        /// <exception cref="ArgumentNullException">Thrown when <paramref name="image"/> is null.</exception>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="image"/> is <c>null</c>.</exception>
         ulong Hash(Image<Rgba32> image);
     }
 }
