1852d911ae702bb002eec6fa1d0d74b3d125d5a7
Coen Munckhof <c.munckhof@gmail.com>
2018-10-28 16:54:22 +0100

    Stylecopping..

--- src/ImageHash/IImageHash.cs
+++ src/ImageHash/IImageHash.cs
@@ -6,12 +6,12 @@
     using SixLabors.ImageSharp.PixelFormats;
 
     /// <summary>
-    /// Interface for perceptual image hashing algorithm
+    /// Interface for perceptual image hashing algorithm.
     /// </summary>
     public interface IImageHash
     {
         /// <summary>Hash the image using the algorithm.</summary>
-        /// <param name="image">image to calculate hash from</param>
+        /// <param name="image">image to calculate hash from.</param>
         /// <returns>hash value of the image.</returns>
         /// <exception cref="ArgumentNullException">Thrown when <paramref name="image"/> is <c>null</c>.</exception>
         ulong Hash(Image<Rgba32> image);
--- tests/ImageHash.Test/CompareHashTest.cs
+++ tests/ImageHash.Test/CompareHashTest.cs
@@ -6,11 +6,11 @@
 
     using Xunit;
 
-    using Sut = CompareHash;
+    using Sut = CoenM.ImageHash.CompareHash;
 
     public class CompareHashTest
     {
-        private const int VALID_HASH_SIZE = 8;
+        private const int ValidHashSize = 8;
 
         [Theory]
         [InlineData(true, false)]
@@ -19,8 +19,8 @@
         public void NullArgumentShouldThrowArgumentNullExceptionTest(bool hash1IsNull, bool hash2IsNull)
         {
             // arrange
-            var hash1 = hash1IsNull ? null : new byte[VALID_HASH_SIZE];
-            var hash2 = hash2IsNull ? null : new byte[VALID_HASH_SIZE];
+            var hash1 = hash1IsNull ? null : new byte[ValidHashSize];
+            var hash2 = hash2IsNull ? null : new byte[ValidHashSize];
 
             // act
             Action act = () => Sut.Similarity(hash1, hash2);
@@ -36,12 +36,12 @@
         [InlineData(9)]
         public void SimilarityThrowsExceptionOnWrongInputSizeArgument(int size)
         {
-            Assert.Throws<ArgumentOutOfRangeException>(() => Sut.Similarity(new byte[size], new byte[VALID_HASH_SIZE]));
-            Assert.Throws<ArgumentOutOfRangeException>(() => Sut.Similarity(new byte[VALID_HASH_SIZE], new byte[size]));
+            Assert.Throws<ArgumentOutOfRangeException>(() => Sut.Similarity(new byte[size], new byte[ValidHashSize]));
+            Assert.Throws<ArgumentOutOfRangeException>(() => Sut.Similarity(new byte[ValidHashSize], new byte[size]));
         }
 
         [Fact]
-        public void OneBitDifferenceShouldResultInAlmost99ProcentSimilarityTest()
+        public void OneBitDifferenceShouldResultInAlmost99PercentSimilarityTest()
         {
             // arrange
             var hash1 = new byte[] { 255, 255, 255, 255, 255, 255, 255, 255 };
@@ -55,7 +55,7 @@
         }
 
         [Fact]
-        public void SameHashShouldResultIn100ProcentSimilarityTest()
+        public void SameHashShouldResultIn100PercentSimilarityTest()
         {
             // arrange
             var hash = new byte[] { 255, 255, 255, 255, 255, 255, 255, 255 };
