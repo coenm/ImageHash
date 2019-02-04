2e24304099b44e28e97a37d176edeccfebbcd0a6
Coen Munckhof <c.munckhof@gmail.com>
2019-02-04 22:40:05 +0100

    forgotten files.

--- demo/Demo/Model/IImageHashSimilarityCalculator.cs
+++ demo/Demo/Model/IImageHashSimilarityCalculator.cs
@@ -4,12 +4,4 @@
     {
         double Calculate(ulong imageHash1, ulong imageHash2);
     }
-
-    public class ImageHashSimilarityCalculator : IImageHashSimilarityCalculator
-    {
-        public double Calculate(ulong imageHash1, ulong imageHash2)
-        {
-            return CoenM.ImageHash.CompareHash.Similarity(imageHash1, imageHash2);
-        }
-    }
 }
--- demo/Demo/ViewModel/DemoViewModel.cs
+++ demo/Demo/ViewModel/DemoViewModel.cs
@@ -8,34 +8,6 @@
     using Microsoft.Expression.Interactivity.Core;
 
 
-    public class FileHashResultsViewModel : ViewModelBase
-    {
-        [NotNull] private readonly IImageHash imageHash;
-
-        public FileHashResultsViewModel([NotNull] IImageHash imageHash)
-        {
-            this.imageHash = imageHash;
-        }
-
-        public ulong AverageHash
-        {
-            get => Properties.Get<ulong>(0);
-            private set => Properties.Set(value);
-        }
-
-        public ulong DifferenceHash
-        {
-            get => Properties.Get<ulong>(0);
-            private set => Properties.Set(value);
-        }
-
-        public ulong PerceptualHash
-        {
-            get => Properties.Get<ulong>(0);
-            private set => Properties.Set(value);
-        }
-    }
-
     public class DemoViewModel : ViewModelBase
     {
         [NotNull] private readonly AverageHash averageHash;
