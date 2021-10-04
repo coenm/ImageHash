b5ee94e46aae9b1f885c85d76c5c2870ec7f75aa
Jesse Trana <jessenkaren@gmail.com>
2021-10-03 21:31:35 -0500

    Cueing up optimization work

---
+++ tests/ImageHash.Test/Algorithms/PerceptualHashOptimizedTest.cs
@@ -0,0 +1,136 @@
+﻿namespace CoenM.ImageHash.Test.Algorithms
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+
+    using CoenM.ImageHash.HashAlgorithms;
+    using CoenM.ImageHash.Test.Internal;
+    using FluentAssertions;
+    using Xunit;
+
+    public class PerceptualHashOptimizedTest
+    {
+        private readonly PerceptualHashOptimized sut;
+
+        private readonly Dictionary<string, ulong> expectedHashes = new Dictionary<string, ulong>
+        {
+            { "Alyson_Hannigan_500x500_0.jpg", 17839858461443178030 },
+            { "Alyson_Hannigan_500x500_1.jpg", 17839823311430827566 },
+            { "Alyson_Hannigan_200x200_0.jpg", 17839858461443178030 },
+            { "Alyson_Hannigan_4x4_0.jpg", 17409736169497899465 },
+            { "github_1.jpg", 13719320793338945348 },
+            { "github_2.jpg", 13783795072850083657 },
+        };
+
+        public PerceptualHashOptimizedTest()
+        {
+            sut = new PerceptualHashOptimized();
+        }
+
+        [Theory]
+        [InlineData("Alyson_Hannigan_500x500_0.jpg", 17839823311430827566)]
+        [InlineData("Alyson_Hannigan_500x500_1.jpg", 17839823311430827566)]
+        [InlineData("Alyson_Hannigan_200x200_0.jpg", 17839823311430827566)]
+        [InlineData("Alyson_Hannigan_4x4_0.jpg", 17409736169531453642)]
+        [InlineData("github_1.jpg", 13719320793338945348)]
+        [InlineData("github_2.jpg", 13783795072850083657)]
+        public void HashImagesTest(string filename, ulong expectedHash)
+        {
+            // arrange
+            ulong result;
+
+            // act
+            using (var stream = TestHelper.OpenStream(filename))
+                result = sut.Hash(stream);
+
+            // assert
+            result.Should().Be(expectedHash);
+        }
+
+        [Fact]
+        [SuppressMessage("ReSharper", "AccessToDisposedClosure", Justification = "Manually reviewed")]
+        public void NotAnImageShouldThrowExceptionTest()
+        {
+            // arrange
+            const string filename = "Not_an_image.txt";
+
+            // act
+            using (var stream = TestHelper.OpenStream(filename))
+            {
+                Action act = () => sut.Hash(stream);
+
+                // assert
+                act.Should().Throw<SixLabors.ImageSharp.UnknownImageFormatException>();
+            }
+        }
+
+        [Fact]
+        public void NullArgumentShouldThrowArgumentNullExceptionTest()
+        {
+            // arrange
+
+            // act
+            Action act = () => sut.Hash(null);
+
+            // assert
+            act.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void ImageWithFilterShouldHaveAlmostOrExactly100Similarity1Test()
+        {
+            // arrange
+            var hash1 = expectedHashes["Alyson_Hannigan_500x500_0.jpg"];
+            var hash2 = expectedHashes["Alyson_Hannigan_500x500_1.jpg"];
+
+            // act
+            var result = CompareHash.Similarity(hash1, hash2);
+
+            // assert
+            result.Should().Be(96.875);
+        }
+
+        [Fact]
+        public void ResizedImageShouldHaveAlmostOrExactly100Similarity2Test()
+        {
+            // arrange
+            var hash1 = expectedHashes["Alyson_Hannigan_500x500_0.jpg"];
+            var hash2 = expectedHashes["Alyson_Hannigan_200x200_0.jpg"];
+
+            // act
+            var result = CompareHash.Similarity(hash1, hash2);
+
+            // assert
+            result.Should().Be(100);
+        }
+
+        [Fact]
+        public void ComparingExtreamlySmallImageShouldDecreaseSimilarityTest()
+        {
+            // arrange
+            var hash1 = expectedHashes["Alyson_Hannigan_4x4_0.jpg"];
+            var hash2 = expectedHashes["Alyson_Hannigan_500x500_0.jpg"];
+
+            // act
+            var result = CompareHash.Similarity(hash1, hash2);
+
+            // assert
+            result.Should().Be(59.375);
+        }
+
+        [Fact]
+        public void TwoDifferentImagesOfGithubArePrettySimilarTests()
+        {
+            // arrange
+            var hash1 = expectedHashes["github_1.jpg"];
+            var hash2 = expectedHashes["github_2.jpg"];
+
+            // act
+            var result = CompareHash.Similarity(hash1, hash2);
+
+            // assert
+            result.Should().Be(71.875);
+        }
+    }
+}
