d4369c2a3a8fe1643f097955e66adc2ed2cbf378
Jaden Young <me@jaden-young.com>
2021-11-06 19:55:18 -0500

    Remove unnecessary using

--- src/ImageHash/HashAlgorithms/PerceptualHash.cs
+++ src/ImageHash/HashAlgorithms/PerceptualHash.cs
@@ -6,7 +6,6 @@
     using System.Linq;
     using System.Numerics;
     using System.Runtime.CompilerServices;
-    using System.Threading.Tasks;
     using SixLabors.ImageSharp;
     using SixLabors.ImageSharp.PixelFormats;
     using SixLabors.ImageSharp.Processing;
