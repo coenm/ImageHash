e200411deed799a99c2ea4c8e9f9f50c46caaa5e
Coen van den Munckhof <c.munckhof@gmail.com>
2021-11-08 18:22:42 +0100

    Use EasyTestFile in test projects (#37)
    Use EasyTestFile in test projects

---
+++ tests/ImageHash.Test/Data/TestData.cs
@@ -0,0 +1,46 @@
+﻿namespace CoenM.ImageHash.Test.Data
+{
+    using System;
+
+    using EasyTestFile;
+    using EasyTestFileXunit;
+
+    public static class TestData
+    {
+        private static readonly EasyTestFileSettings _jpgSettings;
+
+        static TestData()
+        {
+            _jpgSettings = new EasyTestFileSettings();
+            _jpgSettings.UseExtension("jpg");
+        }
+
+        public static TestFile AlysonHannigan200x200_0 => EasyTestFile.Load(_jpgSettings);
+
+        public static TestFile AlysonHannigan4x4_0 => EasyTestFile.Load(_jpgSettings);
+
+        public static TestFile AlysonHannigan500x500_0 => EasyTestFile.Load(_jpgSettings);
+
+        public static TestFile AlysonHannigan500x500_1 => EasyTestFile.Load(_jpgSettings);
+
+        public static TestFile Github_1 => EasyTestFile.Load(_jpgSettings);
+
+        public static TestFile Github_2 => EasyTestFile.Load(_jpgSettings);
+
+        public static TestFile NotAnImage => EasyTestFile.Load();
+
+        public static TestFile GetByName(string name)
+        {
+            return name switch {
+                "Alyson_Hannigan_500x500_0.jpg" => AlysonHannigan500x500_0,
+                "Alyson_Hannigan_500x500_1.jpg" => AlysonHannigan500x500_1,
+                "Alyson_Hannigan_200x200_0.jpg" => AlysonHannigan200x200_0,
+                "Alyson_Hannigan_4x4_0.jpg" => AlysonHannigan4x4_0,
+                "github_1.jpg" => Github_1,
+                "github_2.jpg" => Github_2,
+                "Not_an_image.txt" => NotAnImage,
+                _ => throw new NotImplementedException()
+            };
+        }
+    }
+}
